bc911092046b3796442da7e096172d41be15968b
harkebark <genetafterdark@gmail.com>
2025-02-10 20:08:05 +0000

    more debug output

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -52,7 +52,7 @@ export default function (server: Server, ctx: AppContext, agent: BskyAgent) {
     )
     
 
-    console.log(requesterDid, req)
+    console.log(requesterDid, " requested a feed")
 
     let body: AlgoOutput | undefined = undefined
 
